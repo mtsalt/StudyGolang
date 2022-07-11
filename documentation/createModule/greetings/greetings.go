8d6b18647c3029ad70c59345bb1ba3cb840cb81f
mtsalt <juntaro.shioyama@gmail.com>
2022-07-11 09:16:18 +0900

    add 'add a test'

--- documentation/createModule/greetings/greetings.go
+++ documentation/createModule/greetings/greetings.go
@@ -7,15 +7,46 @@ import (
 	"time"
 )
 
+// // Hello returns a greeting for the named person.
+// func Hello(name string) (string, error) {
+// 	// If no name was given, return an error with message.
+// 	if name == "" {
+// 		return "", errors.New("empty name")
+// 	}
+// 	// Create a message using a random format.
+// 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+// 	return message, nil
+// }
+
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	// If no name was given, return an error with message.
+	// If no name was given, return an error with a message.
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
 	}
 	// Create a message using a random format.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	return message, nil
+}
+
+// Hellos returns a map that associations each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello Ffunction to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // init  sets initial values for variables used in the function.
